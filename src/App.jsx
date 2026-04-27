44dc922b4484880566ed2da9cc08eaa53f644736
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-04-27 20:25:29 +0600

    project initializ

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <div className='text-center my-5 '>
+      <h1 className='btn btn-primary p-10'>Project Started</h1>
+
+    </div>
+      
+     
+    </>
+  )
+}
+
+export default App
