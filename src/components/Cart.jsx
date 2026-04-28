d138d5fe256667119ce0bf6ed2450ab1375187bc
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-04-28 15:50:07 +0600

    no selected card structure

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,20 +1,28 @@
 
 const Cart = ({ carts }) => {
     return (
-        carts.map(cart =>
-            <div >
-                <div key={cart.id} className="mx-auto">
-                    <div className="card w-full bg-base-300 card-xs shadow-sm">
-                        <div className="card-body">
-                            <h2 className="card-title">{cart.name}</h2>
-                            <p>{cart.price}</p>
+        carts.length == 0
+            ?
+            <div>
+                <h1>Your cart is empty</h1>
+                <p>Start exploring to find the perfect tools for your needs!</p>
+            </div>
+
+            :
+            carts.map(cart =>
+
+                <div >
+                    <div key={cart.id} className="mx-auto">
+                        <div className="card w-full bg-base-300 card-xs shadow-sm">
+                            <div className="card-body">
+                                <h2 className="card-title">{cart.name}</h2>
+                                <p>{cart.price}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-            </div>
-
-        )
+            )
 
     );
 };
